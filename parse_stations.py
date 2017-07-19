cc8272f79507a799c201f6f30febea35103dedfc
protream <protream@gmail.com>
2017-07-19 10:29:07 +0800

    add related files

---
+++ parse_stations.py
@@ -0,0 +1,16 @@
+import re
+import requests
+from pprint import pprint
+
+
+def main():
+    url = 'https://kyfw.12306.cn/otn/resources/js/framework/station_name.js?station_version=1.9018'
+    r = requests.get(url, verify=False)
+    pattern = u'([\u4e00-\u9fa5]+)\|([A-Z]+)'
+    stations = dict(re.findall(pattern, r.text))
+    print(stations.keys())
+    print(stations.values())
+
+
+if __name__ == '__main__':
+    main()
